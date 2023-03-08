6e52df159edad1ad7d644ed7ec34ca83f64db347
ThostDk <thomas.osterhammel@gmail.com>
2023-03-08 22:57:43 +0100

    Feature: added Basic display for llist of games
    Need the API to continue

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -1,24 +1,76 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Container, Col, Row} from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import styles from "./GameList.module.css";
-import {useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 const GameList = () => {
-    const navigate = useNavigate();
-    useEffect(() => {
-        
-          navigate("/login");
-    });
-    return (
-        <Container fluid>
-            <Row>
-            <Col lg={4} hidden-xs className={styles.colTestSides}></Col>
-                <Col lg={4} xs={12} className={styles.colTestMid}> test</Col>
-            <Col lg={4} hidden-xs className={styles.colTestSides}></Col>
-            </Row>
-            
-        </Container>
-    );
+  let gameListArray = [
+    {
+      gameTitle: "VegetablesVsZombies (Handicapped)",
+      players: 20,
+      ends: new Date("2023-08-03"),
+    },
+    {
+      gameTitle: "ReneRoyale",
+      players: 20,
+      ends: new Date("2023-08-03"),
+    },
+    {
+      gameTitle: "zzzzZZZZzzzzZ",
+      players: 20,
+      ends: new Date("2023-08-03"),
+    },
+    {
+      gameTitle: "RealGunsAllowed",
+      players: 20,
+      ends: new Date("2023-08-03"),
+    },
+  ];
+
+  //gameListArray.push()
+  //const navigate = useNavigate();
+  //useEffect(() => {
+
+  //      navigate("/login");
+  //});
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg={1} hidden-xs className={styles.colTestSides}></Col>
+        <Col lg={10} xs={12} className={styles.GameList}>
+          {gameListArray.map((game, index) => {
+            return (
+              
+                <Container key={index} className={styles.gameRow}>
+                  <Row>
+                    <Col xs={12}>
+                      <span className={styles.gameTitle}>
+                        Game: {game.gameTitle}
+                      </span>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12}>
+                      <span className={styles.players}> - Players: </span>
+                      {game.players}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12}>
+                      <span className={styles.ends}> - ending Date: </span>
+                      {game.ends.toLocaleDateString()}
+                    </Col>
+                  </Row>
+                </Container>
+              
+            );
+          })}
+        </Col>
+        <Col lg={1} hidden-xs className={styles.colTestSides}></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default GameList;
