32b3caee7cd752269862be42cd588a28f57f24ec
MissSundebo <futhark1536@gmail.com>
2023-03-10 10:02:00 +0100

    Login page html/css

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,16 +1,53 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Container, Col, Row} from "react-bootstrap";
+import {Container, Col, Row, Form, FormGroup, FormControl} from "react-bootstrap";
 import styles from "./LoginForm.module.css";
+import React, {useState} from "react";
+
 const LoginForm = () => {
+    const handleClick = ( ) => {
+        console.log("clicked");
+    };
     return (
-        <Container fluid>
-            <Row>
-            <Col lg={3} hidden-xs className={styles.colTestSides}></Col>
-                <Col lg={6} xs={12} className={styles.colTestMid}> test</Col>
-            <Col lg={3} hidden-xs className={styles.colTestSides}></Col>
-            </Row>
+        <Container fluid className={styles.LoginFormHeaderContainer}>
+            <div>
+                <Form>
+                    <Row>
+                        <Col lg={3} hidden-xs className={styles.colLoginFormSides}></Col>
+
+                        <Col lg={3} xs={12} className={styles.Login}><h2>Login</h2></Col>
+
+                        <Col lg={3} hidden-xs className={styles.colLoginFormSides}></Col>
+
+                        <Col lg={3} xs={8}>
+                            <FormGroup controlId="formUsername">
+                                <Form.Label>Username</Form.Label>
+                                <FormControl type="text" placeholder="Enter your username"/>
+                                </FormGroup>
+                        </Col>    
+                    </Row>
+                    <Row>
+                        
+                        <Col>
+                        <FormGroup controlId="formPassword">
+                                <Form.Label>Password</Form.Label>
+                                <FormControl type="text" placeholder="Enter your password"/>
+                                </FormGroup>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                    <Col lg={4} xs={12}>
+                            <button onClick={handleClick} className={styles.LoginButton}>Login</button>
+                        </Col>
+                    </Row>
+                    
+                </Form>
+            </div>
         </Container>
+        
+        
     );
 };
 
 export default LoginForm;
+
