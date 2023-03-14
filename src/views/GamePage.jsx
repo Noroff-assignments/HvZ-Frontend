136c6b3f637e63ef69cb7e67dd2be1b675b3eb24
ThostDk <44801529+ThostDk@users.noreply.github.com>
2023-03-14 11:39:41 +0100

    feature: added chat placeholder

--- src/views/GamePage.jsx
+++ src/views/GamePage.jsx
@@ -2,20 +2,26 @@ import LandingPageHeader from "../components/Landing/LandingPageHeader";
 import GameLobby from "../components/GameLobby/GameLobby";
 import GameInfo from "../components/Game/GameInfo";
 import GameMap from "../components/Game/GameMap";
+import GameChat from "../components/Game/GameChat";
 import LoginForm from "../components/Login/LoginForm";
 import { Container, Row, Col } from "react-bootstrap";
 
 const GameLobbyPage = () => {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                <Col lg={6} xs={12}><GameInfo></GameInfo></Col>
-                <Col lg={6} xs={12}><GameMap></GameMap></Col>
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col lg={6} xs={12}>
+            <GameInfo />
+            <GameChat />
+          </Col>
+          <Col lg={6} xs={12}>
+            <GameMap />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default GameLobbyPage;
