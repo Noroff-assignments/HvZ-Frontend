a7b3f5e5d17f68f3cb7b11ae1e5bc6e7aaaacdb5
MissSundebo <futhark1536@gmail.com>
2023-03-15 16:13:31 +0100

    Playing with colors

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -2,18 +2,21 @@ import { Container, Col, Row } from "react-bootstrap";
 import styles from "./GameList.module.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch, useLocation, useNavigate, Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
+import React from "react";
 
 const GameList = () => {
   const [clickedGame, setClickedGame] = useState(null);
   const navigate = useNavigate();
   let gameListArray = [
     {
-      gameTitle: "VegetablesVsZombies (Handicapped)",
+      gameTitle: "Vegetables Vs Zombies (Handicapped)",
       players: 20,
       ends: new Date("2023-08-03"),
     },
+    
     {
-      gameTitle: "ReneRoyale",
+      gameTitle: "Rene Royale",
       players: 35,
       ends: new Date("2023-08-03"),
     },
@@ -23,7 +26,7 @@ const GameList = () => {
       ends: new Date("2175-01-09"),
     },
     {
-      gameTitle: "RealGunsAllowed",
+      gameTitle: "Real Guns Allowed",
       players: 20,
       ends: new Date("2023-08-03"),
     },
@@ -34,6 +37,8 @@ const GameList = () => {
     },
   
   ];
+
+  
   useEffect(() => {
     if (clickedGame !==null) {
       navigate("/gameLobby", { state: { currentGame: clickedGame } });
@@ -50,15 +55,14 @@ const GameList = () => {
         <Col lg={4} xs={12} className={styles.GameList}>
           {gameListArray.map((game, index) => {
             return (
+
               <Link
                 key={index}
                 onClick={() => {
                   setClickedGame(game);
-                }}
-      
-              >
+                }}>
                 <Container className={styles.gameRow}>
-                  <Row>
+                  <Row >
                     <Col xs={12}>
                       <h4 className={styles.gameTitle}>
                       {game.gameTitle}
@@ -67,14 +71,12 @@ const GameList = () => {
                   </Row>
                   <Row>
                     <Col xs={12}>
-                      <span className={styles.players}> - Players: </span>
-                      {game.players}
+                      <h6 className={styles.players}> - Players<FaUserAlt/>: {game.players} </h6>
                     </Col>
                   </Row>
                   <Row>
                     <Col xs={12}>
-                      <span className={styles.ends}> - ending Date: </span>
-                      {game.ends.toLocaleDateString()}
+                      <h6 className={styles.ends}> - ending Date: {game.ends.toLocaleDateString()} </h6>
                     </Col>
                   </Row>
                 </Container>
