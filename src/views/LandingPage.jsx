9701e7d265ac71c2f4803d923d5b15db4ef93ae3
MissSundebo <futhark1536@gmail.com>
2023-03-16 13:29:41 +0100

    Alter background

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -7,6 +7,12 @@ const LandingPage = () => {
         <>
             <LandingPageHeader/>
             <GameList/>
+            <div style={{backgroundImage: `url(${"https://cdn.pixabay.com/photo/2015/05/26/16/26/zombie-784914_960_720.jpg"})`,
+        backgroundSize: 'cover',
+        backgroundRepeat:'no-repeat',
+        backgroundPosition:'center',
+        height: '500px' }}>
+        </div>
         </>
     );
 };
