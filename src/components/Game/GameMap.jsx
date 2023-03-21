cce83f0d3d000337c5e54d6873d39e26bc563d95
ThostDk <44801529+ThostDk@users.noreply.github.com>
2023-03-21 09:50:50 +0100

    Cleanup: Removed unused imports

--- src/components/Game/GameMap.jsx
+++ src/components/Game/GameMap.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate, Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Col } from "react-bootstrap";
 import styles from "./GameMap.module.css";
 import { MapContainer } from "react-leaflet/MapContainer";
