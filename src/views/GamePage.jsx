ed1e209976edfafe379243c5ca56f0f8103909ff
ThostDk <44801529+ThostDk@users.noreply.github.com>
2023-03-21 13:08:17 +0100

    Feature: Can generate QR code from string

--- src/views/GamePage.jsx
+++ src/views/GamePage.jsx
@@ -1,5 +1,6 @@
 import GameInfo from "../components/Game/GameInfo";
 import GameMap from "../components/Game/GameMap";
+import KillCodeQR from "../components/Game/KillCodeQR";
 import GameChat from "../components/Game/GameChat";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -11,6 +12,7 @@ const GameLobbyPage = () => {
         <Col sm={12}md={6}lg={{order: 1}} xs={{order: 2}}style={{padding: "0px"}}>
             <GameInfo />
             <GameChat />
+            <KillCodeQR />
           </Col>
           <Col sm={12}md={6}lg={{order: 2}} xs={{order: 1}}style={{padding: "0px"}}>
             <GameMap />
