c9c02f4c83487120abdb6db47519aa5e6ed9ab23
ThostDk <44801529+ThostDk@users.noreply.github.com>
2023-03-21 13:16:43 +0100

    Cleanup Imports

--- src/components/Game/KillCodeQR.jsx
+++ src/components/Game/KillCodeQR.jsx
@@ -1,30 +1,19 @@
 import styles from "./KillCodeQR.module.css";
 import { Col } from "react-bootstrap";
 import React from "react";
-import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
-import { createRoot } from "react-dom/client";
-
-
 
 const KillCodeQR = () => {
   return (
     <Col lg={12} xs={12} className={styles.mapCol}>
-      <div
-        style={{
-          height: "auto",
-          margin: "0 auto",
-          maxWidth: 64,
-          width: "100%",
-        }}
-      >
+      
         <QRCode
           size={256}
           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
           value={"thomasErCool1234"}
           viewBox={`0 0 256 256`}
         />
-      </div>
+      
     </Col>
   );
 };
